e2ae2e148dd1e6001e1b6c94841fcdd3e5956fc0
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-04-01 13:33:48 -0500

    Create salesByMatch.js

---
+++ salesByMatch.js
@@ -0,0 +1,99 @@
+/* There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+Example
+
+
+There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+
+Function Description
+
+Complete the sockMerchant function in the editor below.
+
+sockMerchant has the following parameter(s):
+
+int n: the number of socks in the pile
+int ar[n]: the colors of each sock
+Returns
+
+int: the number of pairs
+Input Format
+
+The first line contains an integer , the number of socks represented in .
+The second line contains  space-separated integers, , the colors of the socks in the pile.
+
+Constraints
+
+ where 
+Sample Input
+
+STDIN                       Function
+-----                       --------
+9                           n = 9
+10 20 20 10 10 30 50 10 20  ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+Sample Output
+
+3
+ */
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+// Given an input int and arr retrun the amount of matched pairs in arr
+// First sort the arr in ascending order
+// Loop through arr equal to n times
+// Create pairs var
+// if arr[i] === arr[i + 1] pairs++ and i++ to move iterator up one
+// return pairs
+
+    let pairs = 0;
+    ar.sort((a, b) => {return a - b});
+
+    for (let i = 0; i < n; i++) {
+        
+        if (ar[i] === ar[i + 1]) {
+            pairs++;
+            i++
+        }
+}
+console.log(pairs);
+return pairs;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = sockMerchant(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
